5a7f94ab699df2acfd67c33e4db45257e0d8ecaf
Mikhail Angelov <mikhail.angelov@gmail.com>
2025-03-09 16:56:14 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,12 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
-  server: {
-    port: 3000,
-  },
+  publicDir: 'public',
   build: {
     outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
   },
 });
