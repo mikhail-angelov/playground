3b1e2611507fc1c342b95f3fd6e1d51723cb196a
Mikhail Angelov <mikhail.angelov@gmail.com>
2025-03-25 21:28:58 +0300

    fix home screen tiles

--- client/src/pages/home/ProjectTile.tsx
+++ client/src/pages/home/ProjectTile.tsx
@@ -15,17 +15,17 @@ const ProjectTile: React.FC<ProjectTileProps> = ({ project, onClick }) => {
       className="bg-gray-800 shadow rounded overflow-hidden hover:shadow-xl hover:scale-105 transition-all duration-300 flex flex-col items-center justify-center min-h-[200px]"
       onClick={() => onClick(project?.projectId)}
     >
-      {project ? (
-        <img
-          src={project.image}
-          alt={project.name}
-          className="max-w-100 h-32 object-cover cursor-pointer"
-        />
-      ) : (
-        <div className="w-100 h-32 bg-gray-700 flex items-center justify-center">
+      <div className="w-100 h-32 flex items-center justify-center">
+        {project ? (
+          <img
+            src={project.image}
+            alt={project.name}
+            className="max-w-100 h-32 object-cover cursor-pointer"
+          />
+        ) : (
           <span className="text-gray-500">No Image</span>
-        </div>
-      )}
+        )}
+      </div>
       <div className="p-2">
         <h3 className="text-sm font-bold truncate text-gray-100">
           {project ? project.name : "Placeholder"}
