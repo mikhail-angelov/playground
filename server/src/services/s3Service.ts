6ce6e11239b6cc48f7564305fc30758e08b4722d
Mikhail Angelov <mikhail.angelov@gmail.com>
2025-04-06 11:35:16 +0300

    fix s3

--- server/src/services/s3Service.ts
+++ server/src/services/s3Service.ts
@@ -9,10 +9,10 @@ import {
 } from "@aws-sdk/client-s3";
 import { Readable } from "stream";
 
-console.log("S3_BUCKET", process.env.S3_SECRET_ACCESS_KEY);
 const getS3 = () => {
   return new S3Client({
     endpoint: process.env.S3_ENDPOINT_URL || "https://storage.yandexcloud.net",
+    region: 'ru-central1',
     credentials: {
       accessKeyId: process.env.S3_ACCESS_KEY_ID || "",
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || "",
